ea3d165fba447d5b5793f4415750ee1ff4fec518
Madhu sudan Bhattarai <summerpalpali@gmail.com>
2023-01-03 19:24:19 +0545

    Add project files.

---
+++ Controllers/RegistrationController.cs
@@ -0,0 +1,56 @@
+﻿using LoginRegistrationApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace LoginRegistrationApplication.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistrationController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public RegistrationController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpPost]
+        [Route("registration")]
+        public string registration(Registration registration)
+        {
+            SqlConnection con = new SqlConnection(_configuration.GetConnectionString("LoginDB").ToString());
+            SqlCommand cmd = new SqlCommand("INSERT INTO Registration(UserName,Password,Email,IsActive) VALUES('" + registration.UserName + "','" + registration.Password + "','" + registration.Email + "', '" + registration.IsActive + "' )", con);
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+            if (i > 0)
+            {
+                return "Data inserted";
+            }
+            else
+            {
+                return "Error";
+            }
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public string login(Registration registration)
+        {
+            SqlConnection con = new SqlConnection(_configuration.GetConnectionString("LoginDB").ToString());
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Registration WHERE Email='" + registration.Email + "' AND Password = '" + registration.Password + "' AND IsActive = '" + registration.IsActive +"' )", con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            if (dt.Rows.Count > 0)
+            {
+                return "Valid User";
+            }
+            else
+            { 
+                return "Invalid User"; 
+            }
+        }
+    }
+}
---
+++ Models/Response.cs
@@ -0,0 +1,8 @@
+﻿namespace LoginRegistrationApplication.Models
+{
+    public class Response
+    {
+        public int statusCode { get; set; }
+        public string statusMessage { get; set; }
+    }
+}
